3b6f83094f7c6ba5f2404f694932f3be65145a39
rikuriku1999 <58874178+rikuriku1999@users.noreply.github.com>
2022-05-28 17:47:16 +0900

    Create messaging.php

---
+++ messaging.php
@@ -0,0 +1,49 @@
+PHP
+define("CHANNEL_ACCESS_TOKEN", 'コンソールで確認');
+define("CHANNEL_SECRET", 'コンソールで確認');
+$contents = file_get_contents('php://input');
+$json = json_decode($contents);
+$event = $json->events[0];
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(CHANNEL_ACCESS_TOKEN);
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => CHANNEL_SECRET]);
+// 個人トークの場合はグループに招待するようにメッセージ表示
+if ($event->source->type != 'group') {
+  $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('グループに招待してください！');
+}
+else {
+  try {
+    // メッセージを書き込んだユーザ情報を取得
+    $response = $bot->getProfile($event->source->userId);
+    if ($response->isSucceeded()) {
+      $profile = $response->getJSONDecodedBody();
+      $user_display_name = $profile['displayName'];
+      $user_picture_url = $profile['pictureUrl'];
+      $user_status_message = $profile['statusMessage'];
+      $fileUrl = "";
+      // メッセージタイプが画像だった場合は画像を保存
+      if ($event->message->type == 'image') {
+        function uploadImageThenGetUrl ($rawBody) {
+          $im = imagecreatefromstring($rawBody);
+          if ($im !== false) {
+            $filename = date("Ymd-His") . '-' . mt_rand() . '.jpg';
+            imagejpeg($im, "images/uploads/" . $filename);
+          }
+          else {
+            error_log("fail to create image.");
+          }
+          return $filename;
+        }
+        $res = $bot->getMessageContent($event->message->id);
+        $fileUrl = uploadImageThenGetUrl($res->getRawBody());
+      }
+    }
+    // ユーザ情報やメッセージをデータベースに保存
+    $pdo = new PDO('mysql:host=ホスト;dbname=DB名;charset=utf8','ユーザ名','パスワード',
+    array(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC));
+    $stmt = $pdo -> prepare("INSERT INTO talks (id, type, group_id, user_id, user_display_name, user_picture_url, user_status_message, talk_type, upload_image_name, time, reply_token, message_id, message_type, message_text, created_at) VALUES ('', ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, NULL)");
+    $stmt->execute(array($event->type, $event->source->groupId, $event->source->userId, $user_display_name, $user_picture_url, $user_status_message, $event->source->type, @$fileUrl, $event->timestamp, $event->replyToken, $event->message->id, $event->message->type, $event->message->text));
+  }
+  catch (PDOException $e) {
+    exit('データベース接続失敗。'.$e->getMessage());
+  }
+}
